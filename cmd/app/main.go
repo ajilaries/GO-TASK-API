75cf02119d1a8b511f2a01f95110b912173d7f4b
Aju <ajilaries20@gmail.com>
2026-04-29 14:47:30 +0530

    main file has updated for the purpose of adding connectivity with the db

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,25 +5,35 @@ import (
 	"net/http"
 
 	"go-task-api/internal/handlers"
+	"go-task-api/pkg/database"
 )
 
 func main() {
-	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			handlers.GetTasks(w, r)
-		} else if r.Method == "POST" {
+	database.ConnectDB()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
 			handlers.CreateTask(w, r)
+		} else if r.Method == http.MethodGet {
+			handlers.GetTasks(w, r)
 		}
 	})
 
-	http.HandleFunc("/tasks/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "PUT" {
-			handlers.UpdateTask(w, r)
-		} else if r.Method == "DELETE" {
-			handlers.DeleteTask(w, r)
-		}
-	})
+	fmt.Println("🚀 Server running on :8080")
+	http.ListenAndServe(":8080", enableCORS(mux))
+}
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	fmt.Println("Server running on :8080 🚀")
-	http.ListenAndServe(":8080", nil)
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
