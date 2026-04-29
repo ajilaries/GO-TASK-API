78d34da2bc43b593c133d664dda6bee1cebd178f
Aju <ajilaries20@gmail.com>
2026-04-29 14:51:43 +0530

    file consisting the operations performing inside task api

--- internal/services/task_service.go
+++ internal/services/task_service.go
@@ -5,18 +5,9 @@ import (
 	"go-task-api/internal/repository"
 )
 
-func GetTasks() []models.Task {
-	return repository.GetAll()
+func CreateTask(task models.Task)error{
+	return repository.CreateTask(task)
 }
-
-func CreateTask(task models.Task) models.Task {
-	return repository.Create(task)
-}
-
-func UpdateTask(id int, task models.Task) (models.Task, bool) {
-	return repository.Update(id, task)
-}
-
-func DeleteTask(id int) bool {
-	return repository.Delete(id)
+func GetTasks() ([]models.Task , error){
+	return repository.GetTasks()
 }
